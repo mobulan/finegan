99971c7f6bc6ee69ccbbde043f10d12e654c098c
莫布兰 <txvxdx@126.com>
2021-12-06 21:22:04 +0800

    1

--- code/miscc/config.py
+++ code/miscc/config.py
@@ -2,9 +2,9 @@ from __future__ import division
 from __future__ import print_function
 
 import os.path as osp
+
 import numpy as np
 from easydict import EasyDict as edict
-
 
 __C = edict()
 cfg = __C
@@ -47,7 +47,6 @@ __C.TRAIN.FLAG = True
 __C.TRAIN.NET_G = ''
 __C.TRAIN.NET_D = ''
 
-
 # Modal options
 __C.GAN = edict()
 __C.GAN.DF_DIM = 64
@@ -57,45 +56,43 @@ __C.GAN.NETWORK_TYPE = 'default'
 __C.GAN.R_NUM = 2
 
 
-
-
 def _merge_a_into_b(a, b):
-    """Merge config dictionary a into config dictionary b, clobbering the
-    options in b whenever they are also specified in a.
-    """
-    if type(a) is not edict:
-        return
+	"""Merge config dictionary a into config dictionary b, clobbering the
+	options in b whenever they are also specified in a.
+	"""
+	if type(a) is not edict:
+		return
 
-    for k, v in a.iteritems():
-        # a must specify keys that are in b
-        if not b.has_key(k):
-            raise KeyError('{} is not a valid config key'.format(k))
+	for k, v in a.iteritems():
+		# a must specify keys that are in b
+		if not b.has_key(k):
+			raise KeyError('{} is not a valid config key'.format(k))
 
-        # the types must match, too
-        old_type = type(b[k])
-        if old_type is not type(v):
-            if isinstance(b[k], np.ndarray):
-                v = np.array(v, dtype=b[k].dtype)
-            else:
-                raise ValueError(('Type mismatch ({} vs. {}) '
-                                  'for config key: {}').format(type(b[k]),
-                                                               type(v), k))
+		# the types must match, too
+		old_type = type(b[k])
+		if old_type is not type(v):
+			if isinstance(b[k], np.ndarray):
+				v = np.array(v, dtype=b[k].dtype)
+			else:
+				raise ValueError(('Type mismatch ({} vs. {}) '
+				                  'for config key: {}').format(type(b[k]),
+				                                               type(v), k))
 
-        # recursively merge dicts
-        if type(v) is edict:
-            try:
-                _merge_a_into_b(a[k], b[k])
-            except:
-                print('Error under config key: {}'.format(k))
-                raise
-        else:
-            b[k] = v
+		# recursively merge dicts
+		if type(v) is edict:
+			try:
+				_merge_a_into_b(a[k], b[k])
+			except:
+				print('Error under config key: {}'.format(k))
+				raise
+		else:
+			b[k] = v
 
 
 def cfg_from_file(filename):
-    """Load a config file and merge it into the default options."""
-    import yaml
-    with open(filename, 'r') as f:
-        yaml_cfg = edict(yaml.load(f))
+	"""Load a config file and merge it into the default options."""
+	import yaml
+	with open(filename, 'r') as f:
+		yaml_cfg = edict(yaml.load(f))
 
-    _merge_a_into_b(yaml_cfg, __C)
+	_merge_a_into_b(yaml_cfg, __C)
